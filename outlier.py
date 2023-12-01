e5bc35b213c0ab34a5fb97918d0f581f50de3bb7
MatthiasBiermanns <M.Biermanns02@gmail.com>
2023-12-01 14:53:06 +0100

    save

---
+++ outlier.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from loguru import logger
+
+class OutlierRemoval(BaseEstimator, TransformerMixin):
+    def __init__(self, strict_removal = False) -> None:
+        self.strict_removal = strict_removal
+
+
+    def fit_transform(self, X, y=None): # called when training
+        _X = X.copy()
+        _X = self.outlier_removal(_X, factor_iqr= 1.5 if self.strict_removal else 3.0 )
+        return _X
+    
+    def transform(self, X, y=None): # called when testing
+        # no implementation, as outliers should not be removed for testing
+        _X = X.copy()
+        return _X
+    
+    def outlier_removal(self, training_df, factor_iqr: float = 3.0):
+        for column in training_df.columns:
+            # parse columns to a numeric data type
+            try:
+                print(column)
+                training_df[column] = pd.to_numeric(training_df[column])
+                # check whether these are of type boolean and parse them if so
+                is_boolean = all(
+                    value in [float(0), float(1)] for value in training_df[column]
+                )
+                if is_boolean:
+                    training_df[column] = training_df[column].astype(bool)
+
+                # else remove outliers using the inter quartile range
+                else:
+                    quantile_value = 0.25
+                    q1 = training_df[column].quantile(quantile_value)
+                    q3 = training_df[column].quantile(1 - quantile_value)
+                    iqr = q3 - q1
+                    lower_bound = q1 - factor_iqr * iqr
+                    upper_bound = q3 + factor_iqr * iqr
+                    # remove outliers
+                    training_df = training_df.loc[
+                        ~(
+                            (training_df[column] < lower_bound)
+                            | (training_df[column] > upper_bound)
+                        )
+                    ]
+
+            # if parsing the column doesn't work the column is a string and can be handled as such
+            except ValueError:
+                training_df[column] = training_df[column].apply(str)
+
+        # parse back all columns to type object
+        for column in training_df.columns:
+            training_df[column] = training_df[column].astype(object)
+
+        logger.info('Outlier removed successfully')
+        return training_df
--- preprocessing.py
+++ preprocessing.py
@@ -45,7 +45,7 @@ from sklearn.compose import ColumnTransformer
 from loguru import logger
 import sklearn
 import re
-
+from outlier import OutlierRemoval
 
 class AbstractNFLPreprocessing(ABC):
     """abstract class to create layout for NFLPreprocessing class"""
@@ -59,6 +59,7 @@ class AbstractNFLPreprocessing(ABC):
         self.run_df = None
         self.pass_df = None
         self.encoder = None
+        self.outlier_remover = None
         self.minmax_scaler = None
         self.standardizer = None
         self.prepro = None
@@ -74,6 +75,7 @@ class AbstractNFLPreprocessing(ABC):
         self.clear_nas(self.pass_df)
         logger.info("Preparing pipeline")
         self.encoder = self.make_encoder()
+        self.outlier_remover = self.make_outlier_remover()
         self.minmax_scaler = self.make_minmax_scaler()
         self.standardizer = self.make_standardizer()
         self.prepro = self.make_preprocessor()
@@ -113,7 +115,7 @@ class AbstractNFLPreprocessing(ABC):
         pass
 
     @abstractmethod
-    def outlier_removal(self, training_df, factor_iqr):
+    def make_outlier_remover(self):
         pass
 
     @abstractmethod
@@ -302,46 +304,6 @@ class NFLPreprocessing(AbstractNFLPreprocessing):
         )
         logger.info("Successfully split into run and pass dataframes")
 
-    def outlier_removal(self, training_df, factor_iqr: float = 3.0):
-        logger.info("Removing outliers")
-        for column in training_df.columns:
-            # parse columns to a numeric data type
-            try:
-                training_df[column] = pd.to_numeric(training_df[column])
-                # check whether these are of type boolean and parse them if so
-                is_boolean = all(
-                    value in [float(0), float(1)] for value in training_df[column]
-                )
-                if is_boolean:
-                    training_df[column] = training_df[column].astype(bool)
-
-                # else remove outliers using the inter quartile range
-                else:
-                    quantile_value = 0.25
-                    q1 = training_df[column].quantile(quantile_value)
-                    q3 = training_df[column].quantile(1 - quantile_value)
-                    iqr = q3 - q1
-                    lower_bound = q1 - factor_iqr * iqr
-                    upper_bound = q3 + factor_iqr * iqr
-                    # remove outliers
-                    training_df = training_df.loc[
-                        ~(
-                            (training_df[column] < lower_bound)
-                            | (training_df[column] > upper_bound)
-                        )
-                    ]
-
-            # if parsing the column doesn't work the column is a string and can be handled as such
-            except ValueError:
-                training_df[column] = training_df[column].apply(str)
-
-        # parse back all columns to type object
-        for column in training_df.columns:
-            training_df[column] = training_df[column].astype(object)
-
-        logger.info("Successfully removed outliers")
-        return training_df
-
     def get_prepro_feature_names_from_pipeline(self) -> list:
         """only works when model has not been added to pipeline!
 
@@ -382,6 +344,10 @@ class NFLPreprocessing(AbstractNFLPreprocessing):
         encoder = Pipeline(steps=[("encoder", OneHotEncoder(drop="first"))])
         return encoder
 
+    def make_outlier_remover(self):
+        outlier_remover = Pipeline(steps=[("outlier_remover", OutlierRemoval())])
+        return outlier_remover
+    
     def make_standardizer(self):
         standardizer = Pipeline(steps=[("standardization", StandardScaler())])
         return standardizer
@@ -405,6 +371,11 @@ class NFLPreprocessing(AbstractNFLPreprocessing):
                         "encoder",
                         self.encoder,
                         encoding_normalization["one_hot_features"],
+                    ),
+                    (
+                        "outlier_remover",
+                        self.outlier_remover,
+                        encoding_normalization["minmax_features"] + encoding_normalization["standardizing_features"], # reuse as all features must get outliers removed
                     ),
                     (
                         "standardization",
