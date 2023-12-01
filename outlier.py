5d482f0e4587192d5a191fe1673aaefcde6bb0b9
MatthiasBiermanns <M.Biermanns02@gmail.com>
2023-12-01 16:49:14 +0100

    class approach

--- outlier.py
+++ outlier.py
@@ -7,9 +7,15 @@ class OutlierRemoval(BaseEstimator, TransformerMixin):
         self.strict_removal = strict_removal
 
 
+    def fit(self, X, y=None):
+        _X = X.copy()
+        _Y = y.copy()
+        _X, _Y = self.outlier_removal(_X, _Y, factor_iqr= 1.5 if self.strict_removal else 3.0 )
+        return _X, _Y
+    
     def fit_transform(self, X, y=None): # called when training
         _X = X.copy()
-        _X = self.outlier_removal(_X, factor_iqr= 1.5 if self.strict_removal else 3.0 )
+        _X = self.outlier_removal(_X, y, factor_iqr= 1.5 if self.strict_removal else 3.0 )
         return _X
     
     def transform(self, X, y=None): # called when testing
@@ -17,11 +23,10 @@ class OutlierRemoval(BaseEstimator, TransformerMixin):
         _X = X.copy()
         return _X
     
-    def outlier_removal(self, training_df, factor_iqr: float = 3.0):
+    def outlier_removal(self, training_df, target, factor_iqr: float = 3.0):
         for column in training_df.columns:
             # parse columns to a numeric data type
             try:
-                print(column)
                 training_df[column] = pd.to_numeric(training_df[column])
                 # check whether these are of type boolean and parse them if so
                 is_boolean = all(
@@ -38,6 +43,7 @@ class OutlierRemoval(BaseEstimator, TransformerMixin):
                     iqr = q3 - q1
                     lower_bound = q1 - factor_iqr * iqr
                     upper_bound = q3 + factor_iqr * iqr
+                    
                     # remove outliers
                     training_df = training_df.loc[
                         ~(
@@ -45,6 +51,8 @@ class OutlierRemoval(BaseEstimator, TransformerMixin):
                             | (training_df[column] > upper_bound)
                         )
                     ]
+                    missing_rows = target.index.difference(training_df.index)
+                    target = target.drop(index=missing_rows)
 
             # if parsing the column doesn't work the column is a string and can be handled as such
             except ValueError:
@@ -55,4 +63,4 @@ class OutlierRemoval(BaseEstimator, TransformerMixin):
             training_df[column] = training_df[column].astype(object)
 
         logger.info('Outlier removed successfully')
-        return training_df
+        return training_df, target
